81b73f10a50b5d241278f4de1af10a21b1e25184
chandra <demonchand@gmail.com>
2015-07-02 23:16:17 +0530

    Index and Form dialog box controllers are added to the scaffold generator.

--- lib/generators/angular/scaffold/scaffold_generator.rb
+++ lib/generators/angular/scaffold/scaffold_generator.rb
@@ -14,9 +14,12 @@ module Angular
 
         controllers_path = "app/assets/javascripts/angular/controllers"
         modules_path = "app/assets/javascripts/angular/services/models"
+        index_path = "app/views/#{plural_name}"
 
         template "controller.js.coffee", controllers_path + "/#{plural_name}_controller.js.coffee"
+        template "dialog_controller.js.coffee", controllers_path + "/#{plural_name}_dialog_controller.js.coffee"
         template "module.js.coffee", modules_path + "/#{scaffold_name}.js.coffee"
+        template "index.html.erb", index_path + "/index.html.erb"
       end
 
       private
